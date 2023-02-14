5d01679e6fea0a09979e91141a2ec8907176bbc0
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2023-02-14 15:47:46 -0300

    Update Boat.js

--- js/Boat.js
+++ js/Boat.js
@@ -9,14 +9,7 @@
 //Dentro da variável options defina para 0.8 o o quanto o corpo do objeto (body) irá "saltar/quicar" após a colisão.
 //Dentro da variável options defina para 1.0 o atrito entre o corpo do objeto(body) e o que ele toca.
 //Dentro da variável options defina para 1.0  o peso do objeto(body).
-class Boat {
-  constructor(x, y, width, height, boatPos, boatAnimation) {
-    var options = {
-      restitution: 0.8,
-      friction: 1.0,
-      density: 1.0,
-      label: "boat"
-    };
+???
     this.animation = boatAnimation;
     this.speed = 0.05;
     this.body = Bodies.rectangle(x, y, width, height, options);
@@ -25,9 +18,9 @@ class Boat {
 
     this.boatPosition = boatPos;
     //Carregue a imagem do barco(boat.png)
-    this.image = loadImage("assets/boat.png");
+    ???
     //Adicione o'this.body' ao mundo
-    World.add(world, this.body);
+    ???
   }
   animate() {
     this.speed += 0.05 % 1.1;
